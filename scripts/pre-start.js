0474177fcb8599b1aa6c2a83d343ba45355b033b
Alcenir Barbosa dos Santos <alcenir.santos.dev@gmail.com>
2025-01-17 10:49:59 -0300

    Fix: Adjust

--- scripts/pre-start.js
+++ scripts/pre-start.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-console.log("Node environment is:", process.env.NODE_ENV);
 if (process.env.NODE_ENV === "production") {
 	const serviceWorkerContents = fs.readFileSync(path.join(__dirname, "../www/sw.js")).toString();
 	const VERSION = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json"))).version;
